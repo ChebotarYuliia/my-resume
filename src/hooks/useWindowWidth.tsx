1752518b80946cc66507b10f0b381b2fd60013c8
ChebotarYuliia <chebotar609@gmail.com>
2025-02-28 16:15:36 +0100

    fix: useWindowWidth

--- src/hooks/useWindowWidth.tsx
+++ src/hooks/useWindowWidth.tsx
@@ -1,9 +1,7 @@
-"use client";
-
 import { useEffect, useState } from "react";
 
 export const useWindowWidth = () => {
-  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [width, setWidth] = useState<number>();
   const [isTablet, setIsTablet] = useState<boolean>(false);
 
   function handleWindowSizeChange() {
@@ -11,14 +9,14 @@ export const useWindowWidth = () => {
     setIsTablet(window.innerWidth < 1024);
   }
   useEffect(() => {
-    if (document && window && typeof window !== "undefined") {
+    if (typeof window !== "undefined") {
       handleWindowSizeChange();
-    }
 
-    window.addEventListener("resize", handleWindowSizeChange);
-    return () => {
-      window.removeEventListener("resize", handleWindowSizeChange);
-    };
+      window.addEventListener("resize", handleWindowSizeChange);
+      return () => {
+        window.removeEventListener("resize", handleWindowSizeChange);
+      };
+    }
   }, []);
 
   return { isTablet, width };
