7b3dbf643e8fab84e23c896655061972bd020afe
ChebotarYuliia <chebotar609@gmail.com>
2025-03-05 12:04:42 +0100

    fix: registerPlugin console warning

--- src/components/scroll-blocks/ScrollBlocks.tsx
+++ src/components/scroll-blocks/ScrollBlocks.tsx
@@ -7,9 +7,6 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import s from "./ScrollBlocks.module.scss";
 
-gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
-gsap.registerPlugin(ScrollTrigger);
-
 type Props = {
   list: Array<string>;
 };
@@ -19,6 +16,9 @@ export const ScrollBlocks = ({ list }: Props) => {
   const textClassName = s.scrollBlocks__text;
   const imgClassName = s.scrollBlocks__img;
   const DURATION = 0.1; // sec
+
+  gsap.registerPlugin(useGSAP); // register the hook to avoid React version discrepancies
+  gsap.registerPlugin(ScrollTrigger);
 
   useGSAP(() => {
     const trigger = ref.current;
@@ -44,14 +44,14 @@ export const ScrollBlocks = ({ list }: Props) => {
 
     imgs.forEach((img, i) => {
       if (imgs[i + 1]) {
-        tl.to(trigger, { durarion: DURATION });
+        tl.to(trigger, { duration: DURATION });
         tl.to(img, { opacity: 0 })
           .to(imgs[i + 1], { opacity: 1 }, "<")
           .to(texts, { yPercent: -(100 * (i + 1)), ease: "none" }, "<");
       }
     });
 
-    tl.to(trigger, { durarion: DURATION });
+    tl.to(trigger, { duration: DURATION });
   });
 
   if (!list.length) {
